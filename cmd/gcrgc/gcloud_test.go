8edc517d0d6681235abf831bedeeb7c355ac9a61
Robin Graillon <robin.graillon@gmail.com>
2019-02-24 23:06:11 +0100

    improved tests and coverage

---
+++ cmd/gcrgc/gcloud_test.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"strings"
+	"testing"
+)
+
+type cliMock struct {
+	cmd *Cmd
+	ret error
+}
+
+func (c *cliMock) Exec(gcmd *Cmd) error {
+	c.cmd = gcmd
+
+	return c.ret
+}
+
+func TestListImages(t *testing.T) {
+	cli := cliMock{nil, nil}
+	gcloud := NewGCloud(&cli)
+
+	gcloud.ListImages("gcr.io/project-id")
+
+	if cli.cmd.name != "gcloud" {
+		t.Errorf("Expected command name to be %s, but got %s", "gcloud", cli.cmd.name)
+	}
+
+	expectedCommand := "container images list --repository=gcr.io/project-id --format=json --limit=999999"
+	actualCommand := strings.Join(cli.cmd.args, " ")
+	if actualCommand != expectedCommand {
+		t.Errorf("Expected command to be \"%s\", but got \"%s\"", expectedCommand, actualCommand)
+	}
+}
+
+func TestListTags(t *testing.T) {
+	cli := cliMock{nil, nil}
+	gcloud := NewGCloud(&cli)
+
+	gcloud.ListTags("gcr.io/project-id/image", "2019-01-01")
+
+	if cli.cmd.name != "gcloud" {
+		t.Errorf("Expected command name to be %s, but got %s", "gcloud", cli.cmd.name)
+	}
+
+	expectedCommand := "container images list-tags gcr.io/project-id/image --format=json --sort-by=TIMESTAMP --limit=999999 --filter=timestamp.datetime<'2019-01-01'"
+	actualCommand := strings.Join(cli.cmd.args, " ")
+	if actualCommand != expectedCommand {
+		t.Errorf("Expected command to be \"%s\", but got \"%s\"", expectedCommand, actualCommand)
+	}
+}
+
+func TestDelete(t *testing.T) {
+	cli := cliMock{nil, nil}
+	gcloud := NewGCloud(&cli)
+	tag := Tag{Digest: "sha256:digest"}
+
+	gcloud.Delete("gcr.io/project-id/image", &tag, false)
+
+	if cli.cmd.name != "gcloud" {
+		t.Errorf("Expected command name to be %s, but got %s", "gcloud", cli.cmd.name)
+	}
+
+	expectedCommand := "container images delete gcr.io/project-id/image@sha256:digest --force-delete-tags --quiet"
+	actualCommand := strings.Join(cli.cmd.args, " ")
+	if actualCommand != expectedCommand {
+		t.Errorf("Expected command to be \"%s\", but got \"%s\"", expectedCommand, actualCommand)
+	}
+
+	if tag.IsRemoved != true {
+		t.Errorf("Expected tag to be marked as deleted")
+	}
+}
+
+func TestDeleteDryRun(t *testing.T) {
+	cli := cliMock{nil, nil}
+	gcloud := NewGCloud(&cli)
+	tag := Tag{Digest: "sha256:digest"}
+
+	gcloud.Delete("gcr.io/project-id/image", &tag, true)
+
+	if cli.cmd != nil {
+		t.Errorf("Unexpected call to gcloud cli")
+	}
+
+	if tag.IsRemoved != true {
+		t.Errorf("Expected tag to be marked as deleted")
+	}
+}
--- cmd/gcrgc/tag_test.go
+++ cmd/gcrgc/tag_test.go
@@ -2,48 +2,43 @@ package main
 
 import "testing"
 
+var containsTagTest = []struct {
+	tag      Tag
+	test     string
+	expected bool
+}{
+	{Tag{}, "some tag", false},
+	{Tag{Tags: []string{"not matching tag"}}, "some tag", false},
+	{Tag{Tags: []string{"not matching tag", "matching tag"}}, "matching tag", true},
+}
+
 func TestContainsTag(t *testing.T) {
-	var tag Tag
-	var expected bool
 	var actual bool
 
-	tag = Tag{}
-	expected = false
-	actual = tag.ContainsTag("some tag")
-	if actual != expected {
-		t.Errorf("Expected ContainsTag to be %v, got %v instead", expected, actual)
-	}
-
-	tag = Tag{Tags: []string{"not matching tag"}}
-	expected = false
-	actual = tag.ContainsTag("matching tag")
-	if actual != expected {
-		t.Errorf("Expected ContainsTag to be %v, got %v instead", expected, actual)
-	}
-
-	tag = Tag{Tags: []string{"not matching tag", "matching tag"}}
-	expected = true
-	actual = tag.ContainsTag("matching tag")
-	if actual != expected {
-		t.Errorf("Expected ContainsTag to be %v, got %v instead", expected, actual)
+	for _, test := range containsTagTest {
+		actual = test.tag.ContainsTag(test.test)
+		if actual != test.expected {
+			t.Errorf("Expected ContainsTag to be %v, got %v instead", test.expected, actual)
+		}
 	}
 }
+
+var isTaggedTest = []struct {
+	tag      Tag
+	expected bool
+}{
+	{Tag{}, false},
+	{Tag{Tags: []string{}}, false},
+	{Tag{Tags: []string{"a tag"}}, true},
+}
+
 func TestIsTagged(t *testing.T) {
-	var tag Tag
-	var expected bool
 	var actual bool
 
-	tag = Tag{}
-	expected = false
-	actual = tag.IsTagged()
-	if actual != expected {
-		t.Errorf("Expected IsTagged to be %v, got %v instead", expected, actual)
-	}
-
-	tag = Tag{Tags: []string{"a tag"}}
-	expected = true
-	actual = tag.IsTagged()
-	if actual != expected {
-		t.Errorf("Expected IsTagged to be %v, got %v instead", expected, actual)
+	for _, test := range isTaggedTest {
+		actual = test.tag.IsTagged()
+		if actual != test.expected {
+			t.Errorf("Expected IsTagged to be %v, got %v instead", test.expected, actual)
+		}
 	}
 }
